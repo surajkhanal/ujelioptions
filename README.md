e385707d455bd60808a60b82eccc8fc9b9daeca2
unknown <suryapsdkhanal@gmail.com>
2018-09-16 17:49:45 +0545

    readme changed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ujelioptions
 
 
-~Current Version:1.4~
+~Current Version:0.0.1~
--- ujelioptions.php
+++ ujelioptions.php
@@ -12,6 +12,7 @@
  
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+include_once('updater.php');
  
 class ElementorCustomElement {
  
@@ -24,6 +25,22 @@ class ElementorCustomElement {
    }
  
    public function init(){
+    if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+      $config = array(
+        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        'proper_folder_name' => 'ujelioptions', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/surajkhanal/ujelioptions', // the GitHub API url of your GitHub repo
+        'raw_url' => 'https://raw.github.com/surajkhanal/ujelioptions/master', // the GitHub raw url of your GitHub repo
+        'github_url' => 'https://github.com/surajkhanal/ujelioptions', // the GitHub url of your GitHub repo
+        'zip_url' => 'https://github.com/surajkhanal/ujelioptions/zipball/master', // the zip url of the GitHub repo
+        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'requires' => '3.0', // which version of WordPress does your plugin require?
+        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+        'readme' => 'README.md', // which file to use as the readme for the version number
+        'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+      );
+      new WP_GitHub_Updater($config);
+    }
       add_action( 'elementor/widgets/widgets_registered', array( $this, 'widgets_registered' ) );
    }
  
